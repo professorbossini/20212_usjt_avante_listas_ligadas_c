2cf09cc45da1a23cd4f7a834b95b3bbc3f41e135
Rodrigo Bossini <31605875+professorbossini@users.noreply.github.com>
2021-09-30 01:08:23 +0000

    insercao no final com ponteiro para o ultimo

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ typedef struct no {
 
 typedef struct lista_ligada_simples {
   struct no * primeiro;
+  struct no * ultimo;
   int qtde;
 }lista_ligada_simples;
 
@@ -15,7 +16,12 @@ lista_ligada_simples * obter_lista (){
   lista_ligada_simples * lista = malloc(sizeof(lista_ligada_simples));
   lista->qtde = 0;
   lista->primeiro = NULL;
+  lista->ultimo = NULL;
   return lista;
+}
+
+int esta_vazia ( lista_ligada_simples * l){
+  return l->qtde <= 0;
 }
 
 void inserir_no_comeco (lista_ligada_simples * lista, int dado){
@@ -23,6 +29,8 @@ void inserir_no_comeco (lista_ligada_simples * lista, int dado){
   novo->dado = dado;
   novo->prox = lista->primeiro;
   lista->primeiro = novo;
+  if (esta_vazia(lista))
+   lista->ultimo = lista->primeiro;
   lista->qtde++;
 }
 
@@ -35,23 +43,81 @@ void exibir_lista (lista_ligada_simples * lista){
 }
 
 int obter (lista_ligada_simples * l, int indice) {
-  if (indice < 0 || indice >= this.qtde) return -1;
-  No * aux = l->primeiro;
+  if (indice < 0 || indice >= l->qtde) return -1;
+  no * aux = l->primeiro;
   for (int i = 0; i < indice; i++, aux = aux->prox);
   return aux->dado;
-
 }
 
 
-int main(void) {
-  srand(time(NULL));
-  lista_ligada_simples * lista = obter_lista();
-  for (int i = 0; i < 100; i++){
-    int dado = rand() % 20 + 1;
-    inserir_no_comeco (lista, dado);
-    exibir_lista(lista);
-    printf ("\n*************************\n");
+
+
+void inserir_no_final (lista_ligada_simples * l, int dado){
+  if (esta_vazia(l))
+    inserir_no_comeco(l, dado);
+  else{
+    no * novo = malloc(sizeof(no));
+    novo->dado = dado;
+    novo->prox = NULL;
+    no * aux = l->primeiro;
+    while (aux->prox != NULL){
+      aux = aux->prox;
+    }
+    aux->prox = novo;
+    l->ultimo = novo;
+    l->qtde++;
   }
-  
-  return 0;
 }
+
+int main (void){
+  srand(time (NULL));
+  lista_ligada_simples * lista = obter_lista();
+  for (int i = 0; i < 10; i++){
+    //sortear o que fazer
+    int inicio_ou_fim = rand() % 2;
+    int dado = rand() % 20 + 1;
+    if (inicio_ou_fim == 0){
+      inserir_no_comeco(lista, dado);
+    }
+    else{
+      inserir_no_final(lista, dado);
+    }
+    exibir_lista(lista);
+    printf ("\n");
+    printf ("lista[%d]=%d\n", lista->qtde - 1, lista->ultimo->dado);
+    
+  }
+  printf ("\n");
+
+}
+
+
+// int main (void){
+//   srand(time (NULL));
+//   lista_ligada_simples * lista = obter_lista();
+//   for (int i = 0; i < 10; i++){
+//     int dado = rand() % 20 + 1;
+//     inserir_no_comeco (lista, dado);
+//     exibir_lista(lista);
+//     printf("\n");
+//     int posicao = rand() % lista->qtde;
+//     printf ("lista[%d]=%d\n", posicao, obter(lista, posicao));
+//     printf ("\n*************************\n");
+//   }
+
+// }
+
+
+
+// int main(void) {
+//   srand(time(NULL));
+//   lista_ligada_simples * lista = obter_lista();
+//   for (int i = 0; i < 100; i++){
+//     int dado = rand() % 20 + 1;
+//     inserir_no_comeco (lista, dado);
+//     exibir_lista(lista);
+//     printf ("\n*************************\n");
+//   }
+  
+//   return 0;
+// }
